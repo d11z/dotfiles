066e76cfa355c9aacecc667e55a29f1ac3d655ec
Deniz Basegmez <dbas12@gmail.com>
2016-05-17 20:27:01 -0400

    Fixed symlinks and made script work from any directory

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-# make sure dotfiles are up to date
+# make sure we are in the right directory
+DIR="$( cd "$( dirname "$BASH_SOURCE}" )" && pwd )"
+cd $DIR
+
+# make sure we are up to date
 git pull origin master
 
 # install software
@@ -11,10 +15,16 @@ git pull origin master
 grep -q '/usr/local/bin/zsh' /etc/shells || echo '/usr/local/bin/zsh' | sudo tee -a /etc/shells
 chsh -s /usr/local/bin/zsh
 
+if [ ! -e ~/.hammerspoon ]; then
+  mkdir ~/.hammerspoon
+fi
+
 # link configs
-mkdir ~/.hammerspoon
-ln -s hammerspoon ~/.hammerspoon/init.lua
-ln -s zshrc ~/.zshrc
-ln -s vimrc ~/.vimrc
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+ln -s $DIR/hammerspoon ~/.hammerspoon/init.lua
+ln -s $DIR/zshrc ~/.zshrc
+ln -s $DIR/vimrc ~/.vimrc
+
+if [ ! -e ~/.vim/autoload/plug.vim ]; then
+  curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
 
